91f94e9cc043ea1cbf63cce9b68e0894f4e28c2b
photowey <photowey@gmail.com>
2024-10-07 16:59:44 +0800

    test: Add: MinioTemplate ut.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ minio-spring-boot-starter-example/src/test/java/io/github/photowey/minio/spring/boot/starter/example/template/MinioTemplateTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.minio.spring.boot.starter.example.template;
+
+import io.github.photowey.minio.spring.boot.starter.example.AbstractTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * {@code MinioTemplateTest}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/10/07
+ */
+@Slf4j
+@SpringBootTest
+class MinioTemplateTest extends AbstractTest {
+
+    @Test
+    void testCreateBuckets() {
+        boolean ok = this.minioTemplate.createBucket("linkinpark");
+        Assertions.assertTrue(ok);
+
+        boolean failed = this.minioTemplate.createBucket("linkinpark");
+        Assertions.assertFalse(failed);
+    }
+
+    @Test
+    void testBucketExists() {
+        boolean exists = this.minioTemplate.bucketExists("linkinpark");
+        Assertions.assertTrue(exists);
+    }
+
+    @Test
+    void testUpload() throws Exception {
+        Resource resource = new ClassPathResource("pictures/linkinpark.jpg");
+        String bucket = "linkinpark";
+        String object = "linkinpark.jpg";
+        this.minioTemplate.putObject(bucket, object, resource.getInputStream());
+        String url = this.minioTemplate.url(bucket, object, 1, TimeUnit.MINUTES);
+        log.info("the minIO access url is: {}", url);
+    }
+
+    @Test
+    void testDownload() throws Exception {
+        String bucket = "linkinpark";
+        String object = "linkinpark.jpg";
+        try (InputStream stream = this.minioTemplate.downloadObject(bucket, object)) {
+            String dir = System.getProperty("user.dir");
+            if (!dir.endsWith(File.separator)) {
+                dir += File.separator;
+            }
+
+            String tmpFile = dir + "linkinpark.jpg";
+            this.write(tmpFile, true, stream.readAllBytes());
+
+            File file = new File(tmpFile);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+}
---
+++ minio-spring-boot3-starter-example/src/test/java/io/github/photowey/minio/spring/boot/starter/example/AbstractTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.minio.spring.boot.starter.example;
+
+import io.github.photowey.minio.spring.boot.autoconfigure.template.MinioTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * {@code AbstractTest}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/10/07
+ */
+public abstract class AbstractTest {
+
+    @Autowired
+    protected MinioTemplate minioTemplate;
+
+    protected void write(final String target, boolean quiet, final byte[] data) {
+        try (RandomAccessFile raf = new RandomAccessFile(target, "rw");
+             FileChannel channel = raf.getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(data.length);
+            buffer.put(data);
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                channel.write(buffer);
+            }
+            channel.force(true);
+        } catch (Exception e) {
+            if (!quiet) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
